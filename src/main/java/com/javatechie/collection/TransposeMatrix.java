49f02286b21110ec42566518a87f6c372e4723e3
anirbancse <acanirban9@gmail.com>
2023-07-13 23:43:06 +0530

    adding new files 13/07/2023

---
+++ src/main/java/com/javatechie/collection/TransposeMatrix.java
@@ -0,0 +1,29 @@
+package com.javatechie.collection;
+
+
+public class TransposeMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 1, 1, 1}, {2, 2, 2, 2}, {3, 3, 3, 3}, {4, 4, 4, 4}};
+
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+
+        int[][] transposedMatrix = new int[columns][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = i + 1; j < columns; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Print the transposed matrix
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/main/java/com/javatechie/java8/GG1.java
@@ -0,0 +1,25 @@
+/*
+package com.javatechie.java8;
+
+import java.util.*;
+
+public class GG1 {
+
+    public static void main(String[] args) {
+        int A[] = {1, 3, 2, 7, 9, 4, 5};
+        Set list = new TreeSet<>();
+        Integer.MAX_VALUE
+
+        List list1 = new ArrayList();
+        list1.stream().sorted().
+        var x = Arrays.stream(A).sum();
+        System.out.println(x);
+        for (int i = 0; i < A.length; i++) {
+            if (x <= A.length * A[i]) {
+                list.add(A[i]);
+            }
+        }
+        System.out.println(list);
+    }
+}
+*/
